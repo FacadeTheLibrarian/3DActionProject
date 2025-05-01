b338dfdb1db886bcdc2844f929c998e87e9c66ea
QuiveringPhantasm <leontopodium1363@gmail.com>
2025-05-02 01:49:42 +0900

    StateMachineBehaviourで攻撃範囲の管理をするための基底クラスを追加

---
+++ Assets/Scripts/Main/Animator/StateMachineActionRoot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+internal class StateMachineActionRoot : StateMachineBehaviour {
+
+    public event Action OnStartAttack;
+    public event Action OnEndAttack;
+
+    private IPlayer _player = default;
+
+    public void Initialization(in IPlayer player) {
+        _player = player;
+    }
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        OnStartAttack?.Invoke();
+    }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        OnEndAttack?.Invoke();
+    }
+}
