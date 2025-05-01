ac5fc13a3a284907d8fc51c5891f92ce6eb2b09a
Yadori_Ouchi <yadoriouchi@gmail.com>
2025-05-02 01:51:17 +0900

    攻撃時のパーティクルを再生するときのenum管理のためのスクリプタブルオブジェクトを追加

---
+++ Assets/Scripts/ScriptableObjectsScripts/AnimationParticleSelecter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AnimationParticleSelecter", menuName = "ScriptableObjects/AnimationParticleSelecter")]
+public class AnimationParticleSelecter : ScriptableObject {
+    public enum e_nagaParticles {
+        bite = 0,
+        flameThrower = 1,
+        leftSlash = 2,
+        rightSlash = 3,
+    }
+}
