3cc6d98fbab461ca75f3c4a385b9874856aa47ec
Yadori_Ouchi <yadoriouchi@gmail.com>
2025-05-04 08:01:01 +0900

    パーティクル再生用のスクリプトを追加

---
+++ Assets/Scripts/Main/Particles/BaseParticlePlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+internal class BaseParticlePlayer : MonoBehaviour {
+    [SerializeField] protected AnimationParticleSelecter _selecter = default;
+    [SerializeField] private List<ParticleSystem> _particles = default;
+
+    private void Start() {
+        foreach (ParticleSystem particle in _particles) {
+            particle.gameObject.SetActive(false);
+        }
+    }
+
+    protected void InnerPlayParticle(int index) {
+        _particles[index].gameObject.SetActive(true);
+        _particles[index].Play();
+    }
+}
---
+++ Assets/Scripts/Main/Particles/SnakeFamily/NagaParticles.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+internal sealed class NagaParticles : BaseParticlePlayer {
+	public void PlayParticle(AnimationParticleSelecter.e_nagaParticles index) {
+		InnerPlayParticle((int)index);
+	}
+}
