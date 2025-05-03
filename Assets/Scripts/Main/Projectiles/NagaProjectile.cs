2f6d46c2e8243c665eeb558362c21b964e7029ea
QuiveringPhantasm <leontopodium1363@gmail.com>
2025-05-04 08:00:08 +0900

    ヘビ族の遠距離攻撃スクリプトを基底クラスから作成

---
+++ Assets/Scripts/Main/Projectiles/NagaProjectile.cs
@@ -0,0 +1,19 @@
+﻿using SimpleMan.VisualRaycast;
+using System.Collections.Generic;
+using UnityEngine;
+
+internal sealed class NagaProjectile : BasePlayerBomberProjectile {
+    [SerializeField] private ParticleSystem _explosion = default;
+    [SerializeField] private float _explosionRadius = 4.0f;
+
+    protected override void OnHitProjectile() {
+        Collider[] results = ComponentExtension.SphereOverlap(this.transform, this.transform.position, _explosionRadius, _layer, true);
+        foreach (Collider collider in results) {
+            if (collider.TryGetComponent(out IDamagableObjects possibleEnemy)) {
+                possibleEnemy.GetHit(_subProjectileDamageAmount, _forward);
+            }
+        }
+        _explosion.gameObject.SetActive(true);
+        _explosion.Play();
+    }
+}
