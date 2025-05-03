926296f5fa6d1adc14e10801b8cb054b54bcf249
Yadori_Ouchi <yadoriouchi@gmail.com>
2025-05-04 07:59:37 +0900

    弾を飛ばす攻撃に対応する弾の基底クラスを作成

---
+++ Assets/Scripts/Main/Projectiles/Base/BasePlayerBomberProjectile.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+internal class BasePlayerBomberProjectile : MonoBehaviour, IPoolableObjects {
+    private bool _isOccupied = false;
+    public bool GetIsOccupied() => _isOccupied;
+
+    [SerializeField] private ParticleSystem _baseProjectileParticleHandler = default;
+
+    [SerializeField] private Vector3 _targetPosition = default;
+    [SerializeField] protected Vector3 _forward = default;
+
+    [SerializeField] private Vector3 _velocity = default;
+
+    [SerializeField] private int _baseProjectileDamageAmount = 0;
+    [SerializeField] protected int _subProjectileDamageAmount = 0;
+
+    [SerializeField] private float _maximumVelocity = 50.0f;
+    [SerializeField] private float _lifetime = 0.0f;
+    [SerializeField] private float _timeLeftToHit = 0.0f;
+    [SerializeField] private bool _isLifeTimeOver = false;
+
+    [SerializeField] protected LayerMask _layer = default;
+
+    public void Fire(in Vector3 currentPosition, in Vector3 forward, in Vector3 staticTargetPosition, in Vector3 initialVelocity, int mainDamage, int subDamage, in LayerMask layer) {
+        _isLifeTimeOver = false;
+        this.transform.position = currentPosition;
+
+        _forward = forward;
+        _targetPosition = staticTargetPosition;
+        _velocity = initialVelocity;
+
+        _baseProjectileDamageAmount = mainDamage;
+        _subProjectileDamageAmount = subDamage;
+
+        _lifetime = _baseProjectileParticleHandler.main.duration;
+        _timeLeftToHit = _lifetime;
+
+        _layer = layer;
+
+        _isOccupied = true;
+        _baseProjectileParticleHandler.Play();
+        StartCoroutine(OnShot());
+    }
+    public void Fire(in Vector3 currentPosition, in Vector3 forward, in Vector3 staticTargetPosition, in Vector3 initialVelocity, int mainDamage, int subDamage, in LayerMask layer, float lifeTime) {
+        _isLifeTimeOver = false;
+        this.transform.position = currentPosition;
+
+        _forward = forward;
+        _targetPosition = staticTargetPosition;
+        _velocity = initialVelocity;
+
+        _baseProjectileDamageAmount = mainDamage;
+        _subProjectileDamageAmount = subDamage;
+
+        _lifetime = lifeTime;
+        _timeLeftToHit = _lifetime;
+
+        _layer = layer;
+
+        _isOccupied = true;
+        _baseProjectileParticleHandler.Play();
+        StartCoroutine(OnShot());
+    }
+
+
+    protected virtual void OnHitProjectile() {
+        //nop
+    }
+
+    private void Homing() {
+        Vector3 currentPosition = this.transform.position;
+
+        Vector3 acceleration = Vector3.zero;
+        Vector3 difference = _targetPosition - currentPosition;
+        acceleration += (difference - _velocity * _timeLeftToHit) * 2.0f / (_timeLeftToHit * _timeLeftToHit);
+
+        _timeLeftToHit -= Time.deltaTime; 
+        if (_lifetime - _timeLeftToHit >= _lifetime) {
+            End();
+            return;
+        }
+        if (_velocity.magnitude < _maximumVelocity) {
+            _velocity += acceleration * Time.deltaTime;
+        }
+        currentPosition += _velocity * Time.deltaTime;
+        this.transform.position = currentPosition;
+    }
+
+    private void End() {
+        _baseProjectileParticleHandler.Stop();
+        _isOccupied = false;
+        _isLifeTimeOver = true;
+    }
+
+    private IEnumerator OnShot() {
+        yield return null;
+        while (true) {
+            Homing();
+            if (_isLifeTimeOver) {
+                OnHitProjectile();
+                yield break;
+            }
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Main/Projectiles/Base/BasePlayerCruisingProjectile.cs
@@ -0,0 +1,91 @@
+﻿using SimpleMan.VisualRaycast;
+using System;
+using System.Collections;
+using UnityEngine;
+
+internal class BasePlayerCruisingProjectile : MonoBehaviour, IPoolableObjects {
+
+    private bool _isOccupied = false;
+    public bool GetIsOccupied() => _isOccupied;
+
+    [SerializeField] private ParticleSystem _baseProjectileParticleHandler = default;
+
+    [SerializeField] protected Vector3 _forward = default;
+    [SerializeField] private Vector3 _velocity = default;
+
+    [SerializeField] private int _baseProjectileDamageAmount = 0;
+    [SerializeField] protected int _subProjectileDamageAmount = 0;
+
+    [SerializeField] private int _seekCounter = 0;
+    [SerializeField] private float _lifetime = 0.0f;
+    [SerializeField] private bool _isLifeTimeOver = false;
+    [SerializeField] private float _seekRadius = 1.0f;
+    [SerializeField] private int _seekInterval = 3;
+
+    [SerializeField] protected LayerMask _layer = default;
+
+    public void Fire(in Vector3 currentPosition, in Vector3 forward, float velocity, int mainDamage, int subDamage, in LayerMask layer) {
+        _isLifeTimeOver = false;
+        this.transform.position = currentPosition;
+
+        _forward = forward;
+        _velocity = forward * velocity;
+
+        _baseProjectileDamageAmount = mainDamage;
+        _subProjectileDamageAmount = subDamage;
+
+        _lifetime = _baseProjectileParticleHandler.main.duration;
+
+        _layer = layer;
+
+        _isOccupied = true;
+        _baseProjectileParticleHandler.Play();
+        StartCoroutine(OnShot());
+    }
+
+    protected virtual void OnHitProjectile() {
+        //nop
+    }
+
+    private void Cruising() {
+        _lifetime -= Time.deltaTime;
+        this.transform.position += _velocity * Time.deltaTime;
+        if (_lifetime <= 0.0f) {
+            End();
+        }
+    }
+
+    private void Seek() {
+        _seekCounter = (_seekCounter + 1) & _seekInterval;
+        if (_seekCounter != _seekInterval) {
+            return;
+        }
+
+        CastResult result = ComponentExtension.SphereCast(this.transform, false, this.transform.position, _forward, _seekRadius, 0.0f, _layer, true);
+        if (result) {
+            if (result.FirstHit.collider.TryGetComponent(out IDamagableObjects possibleEnemy)) {
+                possibleEnemy.GetHit(_baseProjectileDamageAmount, _forward);
+                End();
+                return;
+            }
+        }
+    }
+
+    private void End() {
+        _baseProjectileParticleHandler.Stop();
+        _isOccupied = false;
+        _isLifeTimeOver = true;
+    }
+
+    private IEnumerator OnShot() {
+        while (true) {
+            Cruising();
+            Seek();
+            if (_isLifeTimeOver) {
+                OnHitProjectile();
+                yield break;
+            }
+            yield return null;
+        }
+    }
+}
