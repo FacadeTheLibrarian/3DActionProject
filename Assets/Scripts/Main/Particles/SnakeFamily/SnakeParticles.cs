91530d8f5c9ab773a8904fb7365864a4af61e4dd
QuiveringPhantasm <leontopodium1363@gmail.com>
2025-05-04 08:01:01 +0900

    パーティクル再生用のスクリプトを追加

---
+++ Assets/Scripts/Main/Particles/SnakeFamily/SnakeParticles.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+internal sealed class SnakeParticles : BaseParticlePlayer {
+    public void PlayParticle(AnimationParticleSelecter.e_snakeParticles index) {
+        InnerPlayParticle((int)index);
+    }
+}
---
+++ Assets/Scripts/Main/Particles/SnakeFamily/SnakeletParticles.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+internal sealed class SnakeletParticles : BaseParticlePlayer {
+    public void PlayParticle(AnimationParticleSelecter.e_snakeletParticles index) {
+        InnerPlayParticle((int)index);
+    }
+}
