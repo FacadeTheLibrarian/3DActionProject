637f44595bec821300936bf5bd227ea642ae88ba
Yadori_Ouchi <yadoriouchi@gmail.com>
2025-05-05 00:16:23 +0900

    モデル等のアセット以外のディレクトリを整形

--- Assets/Scripts/Main/IDamagableObjects.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-
-public interface IDamagableObjects {
-	public void GetHit(int damageAmount, in Vector3 playerForward);
-	public Vector3 GetPosition();
-}
---
+++ Assets/Scripts/ScriptableObjectsScripts/MonsterAttackData.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MonsterAttackData", menuName = "ScriptableObjects/MonsterAttackData")]
+public class MonsterAttackData : ScriptableObject {
+    
+}
