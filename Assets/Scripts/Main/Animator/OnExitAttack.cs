26d89e7b5a27bc2efc27688551f718a4e41864b0
Desktop <leontopodium1363@gmail.com>
2025-05-07 01:07:49 +0900

    Revert "プレイヤーのプレハブデータを更新"
    This reverts commit a2f81bc796ed744414acad23c3d5e1080561eefa.

---
+++ Assets/Scripts/Main/Animator/OnExitAttack.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+internal sealed class OnExitAttack : StateMachineBehaviour {
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        animator.ResetTrigger("Attack");
+    }
+}
