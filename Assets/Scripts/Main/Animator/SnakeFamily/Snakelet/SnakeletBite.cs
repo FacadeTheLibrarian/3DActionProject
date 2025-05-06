8953f26039c379c10c97a7ab5a25a7c868f9a4e7
Desktop <leontopodium1363@gmail.com>
2025-05-07 01:18:32 +0900

    no message

---
+++ Assets/Scripts/Main/Animator/SnakeFamily/Snakelet/SnakeletBite.cs
@@ -0,0 +1,18 @@
+﻿using SimpleMan.VisualRaycast;
+using System.Collections.Generic;
+using UnityEngine;
+
+internal sealed class SnakeletBite : BaseAttack {
+    public void Bite() {
+        Vector3 castPosition = GetInitialCastPosition();
+
+        Transform playerTransform = _player.GetTransform();
+        Collider[] results = ComponentExtension.BoxOverlap(playerTransform, castPosition, _boxSize, playerTransform.rotation, _layer, true);
+
+        foreach (Collider collider in results) {
+            if (collider.TryGetComponent<IDamagableObjects>(out IDamagableObjects possibleEnemy)) {
+                possibleEnemy.GetHit((int)(_baseDamage * _player.GetAttackFactor()), _player.GetForward());
+            }
+        }
+    }
+}
--- Assets/Scripts/ScriptableObjectsScripts/MonsterSetData.cs
+++ Assets/Scripts/ScriptableObjectsScripts/MonsterSetData.cs
@@ -3,10 +3,18 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "MonsterSetData", menuName = "ScriptableObjects/Monsters")]
 internal sealed class MonsterSetData : ScriptableObject {
-    [SerializeField] private BasePlayableMonster[] _monsterData = new BasePlayableMonster[(int)BasePlayableMonster.e_generation.max];
-    public BasePlayableMonster this[int index] {
-        get {
-            return _monsterData[index];
-        }
-    }
+    [SerializeField] private GameObject _firstGenerationMonster = default;
+    [SerializeField] private GameObject _secondGenerationMonster = default;
+    [SerializeField] private GameObject _thirdGenerationMonster = default;
+
+    [SerializeField] private PlayerMoveVariables _moveVariables = default;
+    [SerializeField] private PlayerStaminaVariables _staminaVariables = default;
+    [SerializeField] private PlayerAttackVariables _attackVariables = default;
+
+    [SerializeField] private int _requiredExpToSecond = 0;
+    [SerializeField] private int _requiredExpToThird = 0;
+
+    public GameObject GetFirstGenerationMonster => _firstGenerationMonster;
+    public GameObject GetSecondGenerationMonster => _secondGenerationMonster;
+    public GameObject GetThirdGenerationMonster => _thirdGenerationMonster;
 }
