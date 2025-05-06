a2f81bc796ed744414acad23c3d5e1080561eefa
Desktop <leontopodium1363@gmail.com>
2025-05-07 01:07:34 +0900

    プレイヤーのプレハブデータを更新

---
+++ Assets/Scripts/Main/StaticConstants/GlobalConstants.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+internal class GlobalConstants {
+    public enum e_generation {
+        first = 0,
+        second = 1,
+        third = 2,
+        max,
+    }
+}
